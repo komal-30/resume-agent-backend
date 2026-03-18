47004b6983427f1817e0d49a99a7a4b769a0584d
Komal Rane <rane.komalsandy@gmail.com>
2026-03-18 11:20:31 +0000

    Basic Project Structure
    Basic Project Structure

---
+++ src/main/java/com/resumeagent/resume_agent_backend/ResumeAgentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.resumeagent.resume_agent_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeAgentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeAgentBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resumeagent/resume_agent_backend/ResumeAgentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resumeagent.resume_agent_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAgentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
